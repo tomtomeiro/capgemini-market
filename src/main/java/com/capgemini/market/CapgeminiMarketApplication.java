7a70b0230355d78e42fdc4e7868c1a55704a7699
Tomas Flores Garcia <tfloresgarcia94@gmail.com>
2021-08-31 12:28:50 -0500

    Version inicial del proyecto de API market

---
+++ src/main/java/com/capgemini/market/CapgeminiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/market/CapgeminiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgeminiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
